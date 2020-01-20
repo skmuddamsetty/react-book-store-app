527e9a43c634bfdf4230384b35fe8118d495257a
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2020-01-20 06:52:44 -0500

    start with database

--- src/tests/actions/books.test.js
+++ src/tests/actions/books.test.js
@@ -1,4 +1,14 @@
-import { removeBook, editBook, addBook } from '../../actions/books';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import {
+  startAddBook,
+  removeBook,
+  editBook,
+  addBook
+} from '../../actions/books';
+import { books } from '../fixtures/books';
+
+const createMockStore = configureMockStore(thunk);
 
 test('should setup remove book action object', () => {
   const action = removeBook({ id: '123abc' });
@@ -14,19 +24,27 @@ test('should setup edit book action object', () => {
   });
 });
 
-// test('should setup add book action object', () => {
-//   const bookData = {
-//     description: 'First React Book',
-//     price: 3200,
-//     createdAt: 1000,
-//     note: 'This is my first react book'
-//   };
-//   const action = addBook(bookData);
-//   expect(action).toEqual({
-//     type: 'ADD_BOOK',
-//     book: { ...bookData, id: expect.any(String) }
-//   });
-// });
+test('should setup add book action object with provided values', () => {
+  // const bookData = {
+  //   description: 'First React Book',
+  //   price: 3200,
+  //   createdAt: 1000,
+  //   note: 'This is my first react book'
+  // };
+  const action = addBook(books[1]);
+  expect(action).toEqual({
+    type: 'ADD_BOOK',
+    // book: { ...bookData, id: expect.any(String) }
+    book: books[1]
+  });
+});
+
+test('should add book to the database and store', () => {
+  const store = createMockStore({});
+  store.dispatch(startAddBook(books[1]));
+});
+
+test('should add book with defaults to the database and store', () => {});
 
 // test('should setup add book action object with default values', () => {
 //   const action = addBook({});
